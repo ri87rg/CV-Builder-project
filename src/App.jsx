98d1c68f7c4ac426f366b00f843d6747cbe6e0d2
ri87rg <rizoqi2005@gmail.com>
2025-08-28 01:39:44 +0300

    feat: add routes and shared components
    adding routing logic and components shared across paages

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar-components/Navbar";
+import './styling/App.css'
+
+import Home from './pages/home'
+import NotificationsContainer from "./components/notificationsContainer";
+import Notification from "./components/notification";
+import SignInPage from "./pages/signInPage"
+import CVPage from "./pages/cvCreate";
+import CVsPage from './pages/cv_s'
+import { useCVStore } from "./store/cvStore";
+import CV from './components/CVs-components/cv'
+
+function App() {
+  const {CVs} = useCVStore()
+  return (
+    <>
+      <NotificationsContainer />
+        <Navbar />
+        <main>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/sign-in' element={<SignInPage />} />
+              <Route path='/cv/create' element={<CVPage />} />
+              <Route path='/cv' element={<CVsPage />} />
+              {CVs.map((cv) => (
+                  <Route path={`/cv/display/${cv.id}`} element={<CV cv={cv} key={cv.id} />} />
+                ))
+              }
+            </Routes>
+        </main>
+    </>
+  )
+}
+
+export default App
+
