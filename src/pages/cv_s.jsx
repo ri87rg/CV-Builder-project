876e1bff43ea1b699907d002e397ad6d5566decb
ri87rg <rizoqi2005@gmail.com>
2025-08-28 01:42:28 +0300

    feat: add CVs page
    displays all CVs in CV store

---
+++ src/pages/cv_s.jsx
@@ -0,0 +1,14 @@
+import { useCVStore } from "../store/cvStore"
+import CVPreview from "../components/CVs-components/cvPreview"
+
+export default function CVsPage() {
+  const {CVs} = useCVStore()
+  return <div className="flex justify-center items-center">
+    <div className="flex justify-center flex-wrap w-[1020px] h-full gap-6 my-12">
+      {CVs.map((cv) => (
+          <CVPreview cv={cv} key={cv.id}/>
+        ))
+      }
+    </div>
+  </div>
+}
