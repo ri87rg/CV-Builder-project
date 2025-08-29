dee3a32b90110691d648dfab7e6936a174e6e6b4
ri87rg <rizoqi2005@gmail.com>
2025-08-29 16:27:07 +0300

    feat: add shadcn/ui to project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";  // <-- add this
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),  // <-- add this alias
+    },
+  },
 });
