e7473cd33e7012792e7528a9fb06c8f2a37192d9
ri87rg <rizoqi2005@gmail.com>
2025-08-30 19:37:15 +0300

    style: redesign using shadcn/ui components
    New design for sign in/sign up page using shadcn/ui components (Button, Tabs, Card, Label and Input).

--- src/pages/signInPage.jsx
+++ src/pages/signInPage.jsx
@@ -2,100 +2,201 @@ import { useState } from 'react'
 import { useFormik } from 'formik'
 import { useNavigate } from 'react-router-dom'
 import { useAccountStore } from '../store/accountInfo'
-import { useAccountValidation } from '../schemas/signInSchema'
+import { useLoginValidation } from '../schemas/loginSchema'
+import { useRegisterValidation } from '@/schemas/registerScema'
 
-import CustomButton from "../components/customButton"
+import { Label } from "../components/ui/label"
+import { Input } from "../components/ui/input"
+import { Button } from "../components/ui/button"
+import { 
+  Card, 
+  CardHeader, 
+  CardTitle, 
+  CardDescription, 
+  CardAction, 
+  CardContent, 
+  CardFooter
+} from "../components/ui/card"
+import { 
+  Tabs, 
+  TabsList, 
+  TabsTrigger, 
+  TabsContent, 
+} from '../components/ui/tabs'
 
 export default function SignInPage() {
   const navigate = useNavigate()
-  const [viewContent, setViewContent] = useState('sign-in')
   const {logIn, redirectionPath} = useAccountStore()
 
-  const onSubmit = ({username, password}) => {
-    logIn(username, password)
-    navigate(redirectionPath)
-  }
-
-  const {values, errors, touched, handleBlur, handleChange, handleSubmit, handleReset} = useFormik({
+  const loginFormik = useFormik({
     initialValues: {
       username: "",
       password: "",
     },
-    validationSchema: useAccountValidation,
-    onSubmit,
+    validationSchema: useLoginValidation,
+    onSubmit: ({username, password}) => {
+      console.log("submitted")
+      logIn(username, password)
+      navigate(redirectionPath)
+    }
   })
 
-  return <div className={`flex justify-center items-center h-[calc(100vh_-_92.4px)]`}>
-    <div className={`border-2 border-white h-[470px] rounded-2xl w-[400px] md:w-[550px] lg:w-[750px]`}>
-      <div className="w-full flex justify-evenly flex-row">
-        <CustomButton func={() => setViewContent("sign-in")} text={`Sign in`} additionalClasses={`w-full m-3 text-[20px] ${viewContent == 'sign-in' && "glow-effect"}`}></CustomButton>
-        <CustomButton func={() => setViewContent('Register')} text={`Register`} additionalClasses={`w-full m-3 text-[20px] ${viewContent == 'Register' && "glow-effect"}`}></CustomButton>
+  const registerFormik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+      confirmPassword: ""
+    },
+    validationSchema: useRegisterValidation,
+    onSubmit: ({username, password}) => {
+      logIn(username, password)
+      navigate(redirectionPath)
+    }
+  })
+
+  const [currentTab, setCurrentTab] = useState('login')
+  const handleTabChange = (tab) => {
+    if (tab == "login") loginFormik.handleReset()
+    else if (tab == "register") registerFormik.handleReset()
+    setCurrentTab(tab)
+  }
+
+  return (
+    <div className="flex justify-center items-center h-[calc(100vh_-_92px)]">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Tabs value={currentTab} onValueChange={handleTabChange}>
+          <TabsList>
+            <TabsTrigger value="login">Login</TabsTrigger>
+            <TabsTrigger value="register">Register</TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <Card className="w-full max-w-sm">
+              <CardHeader>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>
+                  Login To Get Access To Our Services
+                </CardDescription>
+                <CardAction>
+                  <Button variant="link" onClick={() => handleTabChange('register')}>Sign Up</Button>
+                </CardAction>
+              </CardHeader>
+                <CardContent>
+                  <form onSubmit={loginFormik.handleSubmit}>
+                    <div className="flex flex-col gap-6">
+                      <div className="flex flex-col gap-2">
+                        <Label htmlFor="username">Username</Label>
+                        <Input 
+                          id="username"
+                          name="username"
+                          type="text"
+                          value={loginFormik.values.username}
+                          onChange={loginFormik.handleChange}
+                          onBlur={loginFormik.handleBlur}
+                          placeholder="Username"
+                          className={loginFormik.errors.username && loginFormik.touched.username ? 'border-red-500 border-2' : ''} 
+                        />
+                        {loginFormik.errors.username && loginFormik.touched.username && <p className="text-red-400">{loginFormik.errors.username}</p>}
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <div className="flex items-center">
+                          <Label htmlFor="password">Password</Label>
+                          <a 
+                            href="#" 
+                            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                            >
+                            Forgot Your Password?
+                          </a>
+                        </div>
+                        <Input 
+                          id="password"
+                          name="password"
+                          type="password"
+                          value={loginFormik.values.password}
+                          onChange={loginFormik.handleChange}
+                          onBlur={loginFormik.handleBlur}
+                          placeholder="Password"
+                          className={loginFormik.errors.password && loginFormik.touched.password ? 'border-red-500 border-2' : ''} 
+                        />
+                        {loginFormik.errors.password && loginFormik.touched.password && <p className="text-red-400">{loginFormik.errors.password}</p>}
+                      </div>
+                    </div>
+                  </form>
+                </CardContent>
+                <CardFooter>
+                  <Button type="submit" className="w-full" onClick={loginFormik.handleSubmit}>
+                    Login
+                  </Button>
+                </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="register">
+            <Card className="w-full max-w-sm">
+              <CardHeader>
+                <CardTitle>Register</CardTitle>
+                <CardDescription>
+                  Create Your Account For Free Now!
+                </CardDescription>
+                <CardAction>
+                  <Button variant="link" onClick={() => handleTabChange('login')}>Login</Button>
+                </CardAction>
+              </CardHeader>
+              <CardContent>
+                <form onSubmit={registerFormik.handleSubmit}>
+                  <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-2">
+                      <Label htmlFor="username">Username</Label>
+                      <Input 
+                        id="username"
+                        name="username"
+                        type="text"
+                        value={registerFormik.values.username}
+                        onChange={registerFormik.handleChange}
+                        onBlur={registerFormik.handleBlur}
+                        placeholder="Username"
+                        className={registerFormik.errors.username && registerFormik.touched.username ? 'border-red-500 border-2' : ''} 
+                      />
+                      {registerFormik.errors.username && registerFormik.touched.username && <p className="text-red-400">{registerFormik.errors.username}</p>}
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <Label htmlFor="password">Password</Label>
+                      <Input 
+                        id="password"
+                        name="password"
+                        type="password"
+                        value={registerFormik.values.password}
+                        onChange={registerFormik.handleChange}
+                        onBlur={registerFormik.handleBlur}
+                        placeholder="Password"
+                        className={registerFormik.errors.password && registerFormik.touched.password ? 'border-red-500 border-2' : ''} 
+                      />
+                      {registerFormik.errors.password && registerFormik.touched.password && <p className="text-red-400">{registerFormik.errors.password}</p>}
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <Label htmlFor="password">Confirm Password</Label>
+                      <Input 
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        type="password"
+                        value={registerFormik.values.confirmPassword}
+                        onChange={registerFormik.handleChange}
+                        onBlur={registerFormik.handleBlur}
+                        placeholder="Confirm Password"
+                        className={registerFormik.errors.confirmPassword && registerFormik.touched.confirmPassword ? 'border-red-500 border-2' : ''} 
+                      />
+                      {registerFormik.errors.confirmPassword && registerFormik.touched.confirmPassword && <p className="text-red-400">{registerFormik.errors.confirmPassword}</p>}
+                    </div>
+                  </div>
+                </form>
+              </CardContent>
+              <CardFooter className="flex-col gap-2">
+                <Button type="submit" className="w-full" onClick={registerFormik.handleSubmit}>
+                  Register
+                </Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
       </div>
-
-        <div className=' h-[calc(100%_-_62.5px)] flex justify-center items-center'>
-          <div className="m-4 w-[215px] md:w-[300px] lg:w-[515px] h-[330px]">
-        {viewContent == 'sign-in' ? (
-              <form onSubmit={handleSubmit} onReset={handleReset}>
-                <label>Username</label>
-                <input 
-                value={values.username} 
-                id='username'
-                onChange={handleChange}
-                onBlur={handleBlur}
-                placeholder='enter username'
-                type="text" 
-                className={errors.username && touched.username ? 'border-red-500 border-2' : ''} />
-                {errors.username && touched.username && <p className="text-red-400">{errors.username}</p>}
-
-                <label>Password</label>
-                <input 
-                value={values.password} 
-                id='password'
-                onChange={handleChange}
-                onBlur={handleBlur}
-                placeholder='enter password'
-                type="password" 
-                className={errors.password && touched.password ? 'border-red-500 border-2' : ''} />
-                {errors.password && touched.password && <p className="text-red-400">{errors.password}</p>}
-
-                <div className='flex gap-3 my-4'>
-                  <input type="reset" value='Clear' />
-                  <input type="submit" />
-                </div>
-              </form>
-        ) : viewContent == 'Register' && (
-          <form onSubmit={handleSubmit} onReset={handleReset}>
-                <label>Username</label>
-                <input 
-                value={values.username} 
-                id='username'
-                onChange={handleChange}
-                onBlur={handleBlur}
-                placeholder='enter username'
-                type="text" 
-                className={errors.username && touched.username ? 'border-red-500 border-2' : ''} />
-                {errors.username && touched.username && <p className="text-red-400">{errors.username}</p>}
-
-                <label>Password</label>
-                <input 
-                value={values.password} 
-                id='password'
-                onChange={handleChange}
-                onBlur={handleBlur}
-                placeholder='enter password'
-                type="password" 
-                className={errors.password && touched.password ? 'border-red-500 border-2' : ''} />
-                {errors.password && touched.password && <p className="text-red-400">{errors.password}</p>}
-
-                <div className='flex gap-3 my-4'>
-                  <input type="reset" value='Clear' />
-                  <input type="submit" value='Register' />
-                </div>
-              </form>
-        )}
-          </div>
-        </div>
-
     </div>
-  </div>
+  )
 }
