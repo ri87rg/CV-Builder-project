13c1c4b47bd25aa9bfccfe065265865c07745218
ri87rg <rizoqi2005@gmail.com>
2025-08-30 14:38:41 +0300

    fix: add theme support for sectionHeader styling

--- src/components/sectionHeader.jsx
+++ src/components/sectionHeader.jsx
@@ -1,6 +1,6 @@
 export default function SectionHeader({text="Section Title"}) {
   return <p className='
-      py-1 px-4 text-[13px] text-white border-2 border-White 
+      py-1 px-4 text-[13px] text-black dark:text-white border-2 border-black dark:border-white 
       bg-transparent rounded-2xl font-bold my-6 w-fit pointer-events-none'>
         {text}
       </p>
